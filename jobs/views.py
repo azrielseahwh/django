066cef997208f07a5407d87223c67dd7e9231899
Azriel Seah <azrielseahwh@gmail.com>
2020-11-04 16:14:57 +0800

    committed

--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Job
+import os
+from os import listdir
+from os.path import isfile, join
+from django import template
+register = template.Library()
+@register.filter
+def list_item(lst, i):
+    try:
+        return lst[i]
+    except:
+        return None
 
 # Create your views here.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def list_image_files():
+    IMAGES_DIR = os.path.join(BASE_DIR, 'images')
+    onlyfiles = [join("images", i) for i in listdir(IMAGES_DIR) if isfile(join(IMAGES_DIR, i))]
+    return onlyfiles
+
 def homepage(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs': jobs})
+    return render(request, 'jobs/home.html', {'jobs':jobs,'images':list_image_files})
 
 def detail(request, job_id):
     job_detail = get_object_or_404(Job, pk=job_id)
